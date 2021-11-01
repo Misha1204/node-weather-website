873d6b97692983a7ebf4ed24b565f789c3c632fb
misha1204 <mikheil.kvashali@gmail.com>
2021-11-01 22:21:41 +0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,18 +12,16 @@ weatherForm.addEventListener("submit", (event) => {
 
   const location = search.value;
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = `${data.error}`;
-        } else {
-          messageOne.textContent = `${data.location}`;
-          messageTwo.textContent = `${data.forecast}`;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = `${data.error}`;
+      } else {
+        messageOne.textContent = `${data.location}`;
+        messageTwo.textContent = `${data.forecast}`;
+      }
+    });
+  });
 
   weatherForm.reset();
 });
